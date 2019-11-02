ba18b0079bffb075432fdd0b03e3fa42039b92c2
okkman <okkman@gmail.com>
2019-11-02 22:48:19 +1000

    React material ui

--- React/ReactSetup/src/index.tsx
+++ React/ReactSetup/src/index.tsx
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDom from "react-dom";
 import { GridTest } from './components/GridTest';
 import './styles/index.css';
+import { ThemeProvider } from '@material-ui/core/styles';
 
 ReactDom.render(<GridTest id={5} name={"test"}/>, document.getElementById("app"));
